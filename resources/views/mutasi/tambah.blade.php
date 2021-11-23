88b6eb303f01382460886f81ab96107e97df0990
NurLaila030 <nrlail08@gmail.com>
2021-11-22 22:45:25 -1200

    main

---
+++ resources/views/mutasi/tambah.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas PWB Query Builder</title>
+</head>
+<body>
+
+
+	<h3>Data Mutasi</h3>
+
+	<a href="/mutasi"> Kembali</a>
+
+	<br/>
+	<br/>
+
+	<form action="/mutasi/store" method="post">
+		{{ csrf_field() }}
+		ID Pegawai <input type="number" name="IDPegawai" required="required"> <br/>
+		Departemen <input type="text" name="Departemen" required="required"> <br/>
+		Sub Departemen <input type="text" name="SubDepartemen" required="required"> <br/>
+		Mulai Bertugas <input type="datetime-local" name="MulaiBertugas" required="required"><br/>
+		<input type="submit" value="Simpan Data">
+	</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,22 @@ Route::post('faktorial','ViewController@hitungFaktorial');
 
 //route CRUD
 Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+Route::get('/mutasi','MutasiController@index');
+Route::get('/mutasi/tambah','MutasiController@tambah');
+Route::post('/mutasi/store','MutasiController@store');
+Route::get('/mutasi/edit/{id}','MutasiController@edit');
+Route::post('/mutasi/update','MutasiController@update');
+Route::get('/mutasi/hapus/{id}','MutasiController@hapus');
+
+Route::get('/mutasi1','Mutasi1Controller@index');
+Route::get('/mutasi1/tambah','Mutasi1Controller@tambah');
+Route::post('/mutasi1/store','Mutasi1Controller@store');
+Route::get('/mutasi1/edit/{id}','Mutasi1Controller@edit');
+Route::post('/mutasi1/update','Mutasi1Controller@update');
+Route::get('/mutasi1/hapus/{id}','Mutasi1Controller@hapus');
